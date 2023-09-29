a983254d46113d121c67752cb97d473b12ba548a
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-09-29 18:33:00 -0300

    init db 2

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,4 +1,5 @@
 import os
+from typing import Union
 
 import pandas as pd
 from dotenv import load_dotenv
@@ -10,6 +11,8 @@ load_dotenv()
 
 
 class DataAccessManager:
+    """
+    """
     def __init__(self):
         self.engine = self._create_engine()
         self.Session = sessionmaker(bind=self.engine)
@@ -25,9 +28,24 @@ class DataAccessManager:
         database_url = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
         return sqlalchemy.create_engine(database_url)
 
-    def query_to_dataframe(self, query_result: sqlalchemy.orm.Query) -> pd.DataFrame:
+    def query_to_dataframe(self, query_result: Union[
+        sqlalchemy.orm.Query, sqlalchemy.engine.cursor.CursorResult]) -> pd.DataFrame:
         # Convert the query result to a pandas DataFrame
-        return pd.read_sql(query_result.statement, query_result.session.bind)
+        try:
+            # sql query to dataframe
+            return pd.DataFrame(query_result.fetchall())
+        except AttributeError:
+            # sqlalchemy orm to dataframe
+            return pd.read_sql(query_result.statement, query_result.session.bind)
+
+    def query(self, sql_query: str) -> pd.DataFrame:
+        with self.Session() as session:
+            try:
+                result = session.execute(sqlalchemy.text(sql_query))
+                return self.query_to_dataframe(result)
+            except Exception as e:
+                print(f"Query error: {e}")
+                return pd.DataFrame()
 
 
 if __name__ == '__main__':
@@ -39,7 +57,9 @@ if __name__ == '__main__':
     try:
         result = session.query(dam.main_empresas).all()
 
-        df = dam.query_to_dataframe(session.query(dam.main_empresas).filter_by(razao_social='YANNIS REPRESENTACOES LTDA').one().id)
+        df = dam.query_to_dataframe(
+            session.query(dam.main_empresas).filter_by(id=1))
+        dd = dam.query("SELECT * from main_empresas")
 
         # Print the result
         print(result)
