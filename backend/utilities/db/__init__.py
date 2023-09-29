db871c8483cc1538f94972a991d8869f4e2a3d95
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-09-29 20:42:57 -0300

    add db_access_manager

--- backend/utilities/db/__init__.py
+++ backend/utilities/db/__init__.py
@@ -4,20 +4,17 @@ from typing import Union
 import pandas as pd
 from dotenv import load_dotenv
 import sqlalchemy
-from models import OrmTables
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
 
-class DataAccessManager:
+class DbAccessManager:
     """
     """
     def __init__(self):
         self.engine = self._create_engine()
         self.Session = sessionmaker(bind=self.engine)
-
-        self.main_empresas, self.clients_compts = OrmTables.get_classes().values()
 
     def _create_engine(self):
         MYSQL_HOST = os.getenv('MYSQL_HOST')
@@ -49,21 +46,18 @@ class DataAccessManager:
 
 
 if __name__ == '__main__':
+    from models import OrmTables
     # Initialize the data access manager
     # data_access_manager = dam
-    dam = DataAccessManager()
+    dam = DbAccessManager()
+    main_empresas, clients_compts = OrmTables.get_classes().values()
     # Get ORM classes
-    session = dam.Session()
-    try:
-        result = session.query(dam.main_empresas).all()
+    with dam.Session() as session:
+
+        result = session.query(main_empresas).all()
 
         df = dam.query_to_dataframe(
-            session.query(dam.main_empresas).filter_by(id=1))
+            session.query(main_empresas).filter_by(id=1))
         dd = dam.query("SELECT * from main_empresas")
-
         # Print the result
         print(result)
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        session.close()
