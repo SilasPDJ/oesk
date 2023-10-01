e18f0f292ab6427611d8fea3fef98c18617f54e7
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-10-01 01:01:36 -0300

    preventing data from joins before update

--- backend/repository/__init__.py
+++ backend/repository/__init__.py
@@ -16,6 +16,10 @@ class CustomMethods:
         self.orm = orm
         self.Session = session
 
+    @property
+    def _orm_columns(self) -> list:
+        return [column.name for column in self.orm.__table__.columns]
+
     @staticmethod
     def sort_dataframe(main_df: pd.DataFrame, order_list: list, sorting_key: str) -> pd.DataFrame:
         """Sorts a DataFrame based on a specified order list and a sorting key."""
@@ -30,13 +34,21 @@ class CustomMethods:
         """Updates a database's table based on a dataframe, (allows multiple rows)"""
         with self.Session() as session:
             for row in df.itertuples(index=False):
-                session.merge(self.orm(**row._asdict()))
+                # preventing data from joins
+                data_to_orm = {column: getattr(row, column) for column in self._orm_columns}
+
+                session.merge(self.orm(**data_to_orm))
+                # session.merge(self.orm(**row._asdict()))
             session.commit()
 
     def update_from_dictionary(self, dictionary: dict):
         """update a database's table based on a dictionary, (once per time)"""
         with self.Session() as session:
-            session.merge(self.orm(**dictionary))
+            # preventing data from joins
+            dict_to_orm = {key: dictionary[key] for key in self._orm_columns if key in dictionary}
+
+            session.merge(self.orm(**dict_to_orm))
+            # session.merge(self.orm(**dictionary))
             session.commit()
 
 
@@ -74,12 +86,14 @@ class ClientComptsRepository(CustomMethods):
 
             return self.dba.query_to_dataframe(query)
 
-    def get_ordered_by_imposto_a_calcular(self, order_list: list, allow_lucro_presumido=False,
+    def get_ordered_by_imposto_a_calcular(self, order_list: list = None, allow_lucro_presumido=False,
                                           allow_only_authorized=True) -> pd.DataFrame:
         """
         Retrieve and sort a DataFrame of financial data by 'imposto_a_calcular' column based on a custom order list.
 
         :param order_list: A list specifying the desired sorting order for 'imposto_a_calcular' values.
+                           if None, calls default_order
+
         :param allow_lucro_presumido: If False, filter out rows with 'imposto_a_calcular' value 'LP'
                                       (Lucro Presumido). Default is False.
         :param allow_only_authorized: If True, filter the DataFrame to include only authorized data.
@@ -87,8 +101,9 @@ class ClientComptsRepository(CustomMethods):
         :return: A sorted DataFrame containing financial data with 'imposto_a_calcular' column values
                  ordered according to the provided order list.
 '        """
+        default_order = ["SEM_MOV", "ISS", "ICMS"]
         main_df = self.get_full_df_in_compt(is_authorized=allow_only_authorized)
-        sorted_df = self.sort_dataframe(main_df, order_list, 'imposto_a_calcular')
+        sorted_df = self.sort_dataframe(main_df, order_list or default_order, 'imposto_a_calcular')
 
         if not allow_lucro_presumido:
             sorted_df = sorted_df.loc[sorted_df['imposto_a_calcular'] != 'LP']
@@ -99,7 +114,7 @@ class ClientComptsRepository(CustomMethods):
 
 
 if __name__ == '__main__':
-    cc = ClientComptsRepository(compt='01-2023')
+    cc = ClientComptsRepository(compt='08-2023')
     me = MainEmpresasRepository()
     # df = cc.query(id=2)
     df = cc.get_full_df_in_compt()
@@ -108,10 +123,15 @@ if __name__ == '__main__':
 
     with cc.Session() as session:
         dale = session.query(me.orm).filter_by(id=1)
+        df = cc.get_ordered_by_imposto_a_calcular()
+        df = df.loc[df['main_empresa_id'] == 2]
+        # dale = session.query(cc.orm).filter_by(id=1726)
 
-        df = me.dba.query_to_dataframe(dale)
-        df['razao_social'][0] = "Adeilton 2"
-        me.update_from_dataframe(df)
+        # df = me.dba.query_to_dataframe(dale)
+        # df['razao_social'][0] = "Agathadiesel Mecanica e Manutencao de Veiculos LTDA removeZ"
+        df['valor_total'] = 12
+        # me.update_from_dataframe(df)
+        cc.update_from_dataframe(df)
         for d in dale:
             df = me.dba.query_to_dataframe(d)
             print(d)
