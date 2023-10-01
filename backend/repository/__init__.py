827c71a16723e9a7e6e09c7139597d25a19d21aa
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-10-01 00:21:29 -0300

    add update custom methods

--- backend/repository/__init__.py
+++ backend/repository/__init__.py
@@ -12,8 +12,10 @@ main_empresas, clients_compts = OrmTables.get_classes().values()
 
 
 class CustomMethods:
-    def __init__(self, orm):
+    def __init__(self, orm, session):
         self.orm = orm
+        self.Session = session
+
     @staticmethod
     def sort_dataframe(main_df: pd.DataFrame, order_list: list, sorting_key: str) -> pd.DataFrame:
         """Sorts a DataFrame based on a specified order list and a sorting key."""
@@ -23,13 +25,27 @@ class CustomMethods:
         sorted_df = main_df.sort_values(by='sorting_key').drop(columns='sorting_key')
         return sorted_df
 
+    # update methods
+    def update_from_dataframe(self, df: pd.DataFrame):
+        """Updates a database's table based on a dataframe, (allows multiple rows)"""
+        with self.Session() as session:
+            for row in df.itertuples(index=False):
+                session.merge(self.orm(**row._asdict()))
+            session.commit()
+
+    def update_from_dictionary(self, dictionary: dict):
+        """update a database's table based on a dictionary, (once per time)"""
+        with self.Session() as session:
+            session.merge(self.orm(**dictionary))
+            session.commit()
+
 
 class MainEmpresasRepository(CustomMethods):
     def __init__(self):
         self.dba = DbAccessManager()
         self.Session = self.dba.Session
         self.orm = OrmTables.MainEmpresas
-        super().__init__(self.orm)
+        super().__init__(self.orm, self.Session)
 
 
 class ClientComptsRepository(CustomMethods):
@@ -38,9 +54,10 @@ class ClientComptsRepository(CustomMethods):
         self.dba = DbAccessManager()
         self.Session = self.dba.Session
         self.orm = OrmTables.ClientsCompts
-        super().__init__(self.orm)
+        super().__init__(self.orm, self.Session)
         self.main_empresas = OrmTables.MainEmpresas
 
+    # Queries
     def get_full_df_in_compt(self, is_authorized=False, must_have_status_ativo=True) -> pd.DataFrame:
         """ Get df joining all register fields
         :param is_authorized: pode_declarar in orm
@@ -78,13 +95,28 @@ class ClientComptsRepository(CustomMethods):
 
         return sorted_df
 
+    # Updates...
+
 
 if __name__ == '__main__':
     cc = ClientComptsRepository(compt='01-2023')
+    me = MainEmpresasRepository()
     # df = cc.query(id=2)
     df = cc.get_full_df_in_compt()
 
     df = cc.get_ordered_by_imposto_a_calcular(['ISS', 'ICMS', 'SEM_MOV'])
+
+    with cc.Session() as session:
+        dale = session.query(me.orm).filter_by(id=1)
+
+        df = me.dba.query_to_dataframe(dale)
+        df['razao_social'][0] = "Adeilton 2"
+        me.update_from_dataframe(df)
+        for d in dale:
+            df = me.dba.query_to_dataframe(d)
+            print(d)
+            # d.razao_social = "ADEILTON Teste"
+        session.commit()
     print(df)
 
     print(df)
