08ad228514fb49933e0dc35f0a672bd09f2b3432
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-10-01 14:21:44 -0300

    alphabetical custom_sort + init interface

--- backend/repository/__init__.py
+++ backend/repository/__init__.py
@@ -4,7 +4,7 @@ from utilities.db import DbAccessManager
 from models import OrmTables
 from utilities.compt_utils import get_compt, compt_to_date_obj, calc_date_compt_offset, ate_atual_compt, get_all_valores
 import sqlalchemy as sql
-from typing import Union, List
+from typing import Union, List, Tuple
 
 # OrmTables.MainEmpresas
 # OrmTables.ClientsCompts
@@ -25,9 +25,19 @@ class CustomMethods:
     def sort_dataframe(main_df: pd.DataFrame, order_list: list, sorting_key: str) -> pd.DataFrame:
         """Sorts a DataFrame based on a specified order list and a sorting key."""
 
-        main_df['sorting_key'] = main_df[sorting_key].apply(
-            lambda x: order_list.index(x) if x in order_list else len(order_list))
+        def custom_sort(value):
+            if value in order_list:
+                return order_list.index(value), value
+            else:
+                # Custom alphabetical sorting
+                return len(order_list), value
+
+        # Apply the custom sorting function to the sorting_key column
+        main_df['sorting_key'] = main_df[sorting_key].apply(custom_sort)
+
+        # Sort the DataFrame based on the sorting_key
         sorted_df = main_df.sort_values(by='sorting_key').drop(columns='sorting_key')
+
         return sorted_df
 
     # update methods
@@ -71,8 +81,8 @@ class ClientComptsRepository(CustomMethods):
         self.main_empresas = OrmTables.MainEmpresas
 
     # Queries
-    def get_full_data_in_compt(self, is_authorized=False, must_have_status_ativo=True, to_df=True) -> Union[
-        pd.DataFrame, List[sql]]:
+    def _get_full_data_in_compt(self, is_authorized=False, must_have_status_ativo=True, to_df=True) -> Union[
+                                pd.DataFrame, List[sql.orm.Query]]:
         """Get a DataFrame or a list of ORM queries by joining all registered fields.
 
         :param is_authorized: Filter by 'pode_declarar' in the ORM.
@@ -92,14 +102,15 @@ class ClientComptsRepository(CustomMethods):
             else:
                 return query.all()
 
-    def get_ordered_by_imposto_a_calcular(self, order_list: list = None, allow_lucro_presumido=False,
-                                          allow_only_authorized=True) -> pd.DataFrame:
+    def _get_ordered_by_imposto_a_calcular(self, sorting_list: list = None, allowing_list: list = None,
+                                           allow_lucro_presumido=False,
+                                           allow_only_authorized=True) -> pd.DataFrame:
         """
         Retrieve and sort a DataFrame of financial data by 'imposto_a_calcular' column based on a custom order list.
 
-        :param order_list: A list specifying the desired sorting order for 'imposto_a_calcular' values.
+        :param sorting_list: A list specifying the desired sorting order for 'imposto_a_calcular' values.
                            if None, calls default_order
-
+        :param allowing_list: A list containing the 'imposto_a_calcular' that should be in data
         :param allow_lucro_presumido: If False, filter out rows with 'imposto_a_calcular' value 'LP'
                                       (Lucro Presumido). Default is False.
         :param allow_only_authorized: If True, filter the DataFrame to include only authorized data.
@@ -108,13 +119,27 @@ class ClientComptsRepository(CustomMethods):
                  ordered according to the provided order list.
 '        """
         default_order = ["SEM_MOV", "ISS", "ICMS"]
-        main_df = self.get_full_data_in_compt(is_authorized=allow_only_authorized)
-        sorted_df = self.sort_dataframe(main_df, order_list or default_order, 'imposto_a_calcular')
+        main_df = self._get_full_data_in_compt(is_authorized=allow_only_authorized)
+        sorted_df = self.sort_dataframe(main_df, sorting_list or default_order, 'imposto_a_calcular')
 
         if not allow_lucro_presumido:
             sorted_df = sorted_df.loc[sorted_df['imposto_a_calcular'] != 'LP']
 
+        if allowing_list:
+            sorted_df = sorted_df.loc[sorted_df['imposto_a_calcular'].isin(allowing_list)]
+
         return sorted_df
+
+    def get_g5_df(self) -> Tuple[list, pd.DataFrame]:
+        df = self._get_ordered_by_imposto_a_calcular(sorting_list=['ISS'])
+        df = self._get_ordered_by_imposto_a_calcular(allowing_list=['ISS'])
+
+        attributes_required = ['razao_social', 'cnpj', 'cpf',
+                               'codigo_simples', 'valor_total', 'imposto_a_calcular', 'nf_saidas', 'nf_entradas']
+        df_required = df[attributes_required]
+        # TODO: separar G5 por ISS e ICMS
+        print(df)
+
 
     # Updates...
 
@@ -123,26 +148,4 @@ if __name__ == '__main__':
     cc = ClientComptsRepository(compt='08-2023')
     me = MainEmpresasRepository()
     # df = cc.query(id=2)
-    df = cc.get_full_data_in_compt()
-
-    df = cc.get_ordered_by_imposto_a_calcular(['ISS', 'ICMS', 'SEM_MOV'])
-
-    with cc.Session() as session:
-        dale = session.query(me.orm).filter_by(id=1)
-        df = cc.get_ordered_by_imposto_a_calcular()
-        df = df.loc[df['main_empresa_id'] == 2]
-        # dale = session.query(cc.orm).filter_by(id=1726)
-
-        # df = me.dba.query_to_dataframe(dale)
-        # df['razao_social'][0] = "Agathadiesel Mecanica e Manutencao de Veiculos LTDA removeZ"
-        df['valor_total'] = 12
-        # me.update_from_dataframe(df)
-        cc.update_from_dataframe(df)
-        for d in dale:
-            df = me.dba.query_to_dataframe(d)
-            print(d)
-            # d.razao_social = "ADEILTON Teste"
-        session.commit()
-    print(df)
-
-    print(df)
+    cc.get_g5_df()
