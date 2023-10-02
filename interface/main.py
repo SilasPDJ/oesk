3779d966ac4e5a7ee5e858ee2fcaad25b5ecc0ae
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-10-02 14:13:41 -0300

    improve styling + displaying clients

--- interface/main.py
+++ interface/main.py
@@ -41,16 +41,16 @@ class App(ctk.CTk):
         main_frame = ctk.CTkFrame(self, corner_radius=0)
         main_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
 
-        label = ctk.CTkLabel(main_frame, text="Funções Principais",
+        label_1 = ctk.CTkLabel(main_frame, text="Funções Principais",
                                        font=ctk.CTkFont(size=20, weight="bold"))
-        label.grid(row=0, column=1, rowspan=1, columnspan=4, pady=10)
+        label_1.grid(row=0, column=0, rowspan=1, columnspan=4, pady=10)
 
-        cols = 2
-        frames = []
-        for col in range(1, cols):
-            frame = ctk.CTkFrame(main_frame, corner_radius=0)
-            frame.grid(row=1, column=col, rowspan=4, sticky="nsew")
-            frames.append(frame)
+        label_2 = ctk.CTkLabel(main_frame, text="Funções Principais",
+                                       font=ctk.CTkFont(size=20, weight="bold"))
+        label_2.grid(row=0, column=0, rowspan=1, columnspan=4, pady=10)
+
+        frame = ctk.CTkFrame(main_frame, corner_radius=0)
+        frame.grid(row=1, column=0, rowspan=4, sticky="nsew")
 
         button_data_0 = [
             self._set_button_data(call_ginfess, 'Fazer Ginfess'
@@ -83,33 +83,37 @@ class App(ctk.CTk):
 
         ]
 
-        for col, button_info in enumerate(button_data_0):
+        for row, button_info in enumerate(button_data_0):
             text = button_info['text']
             function = button_info['function']
             text_color = button_info['text_color']
             fg_color = button_info['fg_color']
             hover_color = button_info['hover_color']
 
-            button = ctk.CTkButton(frames[0], text=text, command=function,
+            button = ctk.CTkButton(frame, text=text, command=function,
                                              fg_color=fg_color, text_color=text_color, hover_color=hover_color)
 
-            button.grid(row=col, column=0, padx=20, pady=10)
+            button.grid(row=row, column=0, padx=20, pady=10)
 
-        for col, button_info in enumerate(button_data_1):
+        for row, button_info in enumerate(button_data_1):
             text = button_info['text']
             function = button_info['function']
             text_color = button_info['text_color']
             fg_color = button_info['fg_color']
             hover_color = button_info['hover_color']
 
-            button = ctk.CTkButton(frames[0], text=text, command=function,
+            button = ctk.CTkButton(frame, text=text, command=function,
                                              fg_color=fg_color, text_color=text_color, hover_color=hover_color)
 
-            button.grid(row=col, column=1, padx=20, pady=10)
+            button.grid(row=row, column=1, padx=20, pady=10)
 
     def crate_helpy_methods_frame(self):
-        scrollable_frame = ctk.CTkScrollableFrame(self, label_text="Selecione as opções", label_font=('sans-serif',16))
-        scrollable_frame.grid(row=0, column=2, padx=(10, 10), pady=(5, 10), sticky="nsew")
+        # TODO: create a main frame for both
+        main_frame = ctk.CTkFrame(self, width=180)
+        main_frame.grid(row=0, column=2,padx=(20, 10), pady=(5, 0), sticky="nsew")
+
+        scrollable_frame = ctk.CTkScrollableFrame(main_frame, label_text="Selecione as opções", label_font=('sans-serif',16))
+        scrollable_frame.grid()
 
         options = ["ISS", "ICMS", "SEM_MOV", "LP"]
 
@@ -121,41 +125,20 @@ class App(ctk.CTk):
             if options[i] != options[-1]:
                 switch.select()
 
-
-        # funcoes pt1
-        frame = ctk.CTkFrame(self, width=180, corner_radius=0)
-        frame.grid(row=0, column=3, rowspan=4, sticky="nsew")
-        button_data = [
-            self._set_button_data(abre_pasta, 'Abre/copia pasta [F1]'
-                                  ),
-            self._set_button_data(copy_data_to_clipboard, 'Copia Campo [F4]',
-                                  ),
-        ]
-        for i, button_info in enumerate(button_data):
-            text = button_info['text']
-            function = button_info['function']
-            text_color = button_info['text_color']
-            fg_color = button_info['fg_color']
-            hover_color = button_info['hover_color']
-            button = ctk.CTkButton(frame, text=text, command=function,
-                                             fg_color=fg_color, text_color=text_color, hover_color=hover_color)
-
-            button.grid(row=i, column=0, padx=20, pady=10)
-
     def display_clientes(self):
-        listbox_frame = ctk.CTkFrame(self, width=100)
+        listbox_frame = ctk.CTkFrame(self, width=200)
+        listbox_frame.grid(row=0, column=3, padx=(10, 10), pady=(5, 10), sticky="nsew")
         label = ctk.CTkLabel(listbox_frame, text="Selecione clientes",
                                        font=ctk.CTkFont(size=20, weight="bold"))
-        label.grid(row=0, column=0, rowspan=1, pady=10)
+        label.grid(row=0, column=0, rowspan=1, columnspan=2, pady=10)
 
-        listbox_frame.grid(row=0, column=3, padx=(10, 10), pady=(5, 10), sticky="nsew")
         df = self.client_repository.get_interface_df()
 
         listvariable = df['razao_social'].to_list()
         listvariable = tk.StringVar(value=listvariable)
 
-
-        listbox = CTkListbox(listbox_frame, command=lambda x: print(x), text_color="#000", listvariable=listvariable)
+        listbox = CTkListbox(listbox_frame, command=lambda x: print(x), text_color="#000", listvariable=listvariable,
+                             width=300, height=275)
         listbox.bind("<Down>", lambda event: self._on_keyup_keydown(listbox, 1))
         listbox.bind("<Up>", lambda event: self._on_keyup_keydown(listbox, -1))
         listbox.grid(row=1, column=0)
@@ -164,6 +147,7 @@ class App(ctk.CTk):
         current_index = widget.curselection()
         next_index = (current_index + direction) % widget.size()
         widget.activate(next_index)
+
 
 if __name__ == "__main__":
     app = App()
