4fbeae7a9549e013d91331b0a60e2796c5b12725
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-10-03 01:50:47 -0300

    change_switch_options_selection + display_categoria_clientes **

--- interface/actions/__init__.py
+++ interface/actions/__init__.py
@@ -2,40 +2,48 @@ import os
 import clipboard
 import subprocess
 
-def abre_pasta(self, event=None):
-    folder = InitialSetting.files_pathit(self.selected_client.get(), COMPT)
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    subprocess.Popen(f'explorer "{folder}"')
-    clipboard.copy(folder)
+class Actions:
+    def __init__(self, app_instance):
+        # composição...
+        self.app = app_instance
+        self.clients_permited = self.app.allowed_clients
+        self.client_compts_df = self.app.client_compts_df
 
 
-def copy_data_to_clipboard(self, event=None):
-    campo = event.widget.get() if event else ""
-    if campo == '':
-        campo = "cnpj"
-    found_object = self.EMPRESAS_ORM_OPERATIONS.filter_by_razao_social(
-        self.selected_client.get())
-    returned = getattr(found_object, campo)
-    clipboard.copy(returned)
-    return returned
+    def abre_pasta(self, event=None):
+        folder = InitialSetting.files_pathit(self.selected_client.get(), COMPT)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        subprocess.Popen(f'explorer "{folder}"')
+        clipboard.copy(folder)
 
-def call_simples_nacional():
-    print('sn')
-def call_gias():
-    print('gias')
-def call_ginfess():
-    pass
 
-def call_giss():
-    pass
+    def copy_data_to_clipboard(self, event=None):
+        campo = event.widget.get() if event else ""
+        if campo == '':
+            campo = "cnpj"
+        found_object = self.EMPRESAS_ORM_OPERATIONS.filter_by_razao_social(
+            self.selected_client.get())
+        returned = getattr(found_object, campo)
+        clipboard.copy(returned)
+        return returned
 
-def call_g5():
-    pass
-def call_func_v3(a):
-    print(a)
+    def call_simples_nacional(self):
+        print('sn')
+    def call_gias(self):
+        print('gias')
+    def call_ginfess(self):
+        pass
 
-def call_send_pgdas_email():
-    pass
-    print('pgdas email bt')
+    def call_giss(self):
+        pass
+
+    def call_g5(self):
+        pass
+    def call_func_v3(selfa):
+        print(a)
+
+    def call_send_pgdas_email(self,a):
+        pass
+        print('pgdas email bt')
 
