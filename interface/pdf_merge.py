9d6ed1c88269e27bd45a5f937ad955cfb4f66640
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-10-21 00:52:20 -0300

    fix _mergir_pdfs + add executable

--- interface/pdf_merge.py
+++ interface/pdf_merge.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog
+from typing import Union
 
 import customtkinter as ctk
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfMerger
 import os
 from tkinter.filedialog import askdirectory, askopenfilenames
 
@@ -23,37 +24,42 @@ class App(ctk.CTk):
         bt_mergir_specifics = ctk.CTkButton(self.frame, text="Mergir PDFs específicos",
                                             command=self.select_pdfs_location_by_selecting_files,
                                             fg_color="brown")
+        self.arquivo_final_nome = tk.StringVar(value='arquivosJuntosPdf')
 
-        self.path_stringvar = tk.StringVar()
+        arquivo_final_label = ctk.CTkLabel(self, text="Nome do arquivo que irá unir os PDFs: ")
+
+        arquivo_final_entry = ctk.CTkEntry(self, textvariable=self.arquivo_final_nome)
 
         # Add padding (margin) between buttons
         bt_mergir_specifics.grid(row=0, column=0, pady=10, padx=10)
         bt_mergir_all.grid(row=0, column=1, pady=10, padx=10)
+        # Center the label and entry widget vertically and horizontally
+        arquivo_final_label.grid(row=1, column=0, sticky="nsew")
+        arquivo_final_entry.grid(row=2, column=0, sticky="nsew")
 
     def select_pdfs_location_by_dir(self):
         dirpath = filedialog.askdirectory(initialdir=self.initial_dialog_dir, title='diretorio completo')
         if dirpath:
-            self.path_stringvar.set(dirpath)
-            self._mergir_pdfs()
+            self._mergir_pdfs(dirpath)
 
     def select_pdfs_location_by_selecting_files(self):
         paths = filedialog.askopenfilenames(initialdir=self.initial_dialog_dir, title='arquivos PDF específicos')
         if paths:
-            self.path_stringvar.set("\n".join(paths))  # Convert to a string
-            print(self.path_stringvar.get())
-            self._mergir_pdfs()
-    def _mergir_pdfs(self):
-        path = self.path_stringvar.get()
-        try:
-            files = eval(path)
-        except (SyntaxError, Exception):
-            files = [os.path.join(path, file)
-                     for file in os.listdir(path) if file.upper().endswith('')]
+            self._mergir_pdfs(paths)
+
+    def _mergir_pdfs(self, path: Union[os.PathLike, tuple]):
+        if isinstance(path, tuple):
+            # é uma tupla com o caminho completo ja
+            files = [p for p in path]
+            path = os.path.dirname(files[0])
+
+        else:
+            files = [(os.path.join(path, file)).replace('/', '\\')
+                     for file in os.listdir(path) if file.upper().endswith('PDF')]
 
         files.sort(reverse=True)
 
-        input(files)
-        merger = PdfFileMerger()
+        merger = PdfMerger()
 
         # pdfs = [for pdf in pdfs]
         pdfs = files
@@ -61,7 +67,7 @@ class App(ctk.CTk):
         for pdf in pdfs:
             merger.append(pdf)
 
-        merger.write(os.path.join(path, "ArquivosJuntos.pdf"))
+        merger.write(os.path.join(path, self.arquivo_final_nome.get() + '.pdf'))
         merger.close()
 
 
