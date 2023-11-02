d2f2c6e245bff501a3aafa207b0af5269d28ad76
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-11-02 15:24:06 -0300

    adjust filter

--- interface/autocomplete_listbox/__init__.py
+++ interface/autocomplete_listbox/__init__.py
@@ -4,14 +4,16 @@ from typing import Union
 import customtkinter as ctk
 from CTkListbox import *
 
+
 class AutoCompleteListbox:
-    def __init__(self, frame: tk.Frame, current_client_selection: Union[tk.Listbox, CTkListbox], allowed_clients:list,
+    def __init__(self, frame: tk.Frame, listbox_client_selection: Union[tk.Listbox, CTkListbox], allowed_clients:list,
                  grid_elements=True):
-        self.current_client_selection = current_client_selection
+        self.listbox_client_selection = listbox_client_selection
         self.allowed_clients = allowed_clients
+        self.original_items = self.allowed_clients  # cópia dos elementos originais
 
         for item in self.allowed_clients:
-            self.current_client_selection.insert(tk.END, item)
+            self.listbox_client_selection.insert(tk.END, item)
 
         self.entry = ctk.CTkEntry(frame)
         self.entry.bind("<KeyRelease>", self.filter_listbox)
@@ -21,11 +23,16 @@ class AutoCompleteListbox:
     def filter_listbox(self, event):
         filter_text = self.entry.get().lower()
         try:
-            self.current_client_selection.delete(0, tk.END)  # Limpar a lista
+            self.listbox_client_selection.delete(0, tk.END)  # Limpar a lista
         except IndexError:
             pass
         else:
-            for looping_item in self.allowed_clients:
-                _item = looping_item.lower()
-                if any(word in _item for word in filter_text.split()):
-                    self.current_client_selection.insert(tk.END, looping_item)
+            if not filter_text:  # Se o filtro estiver vazio, restaure a lista original
+                for item in self.original_items:
+                    self.listbox_client_selection.insert(tk.END, item)
+            else:
+                for looping_item in self.original_items:
+                    _item = looping_item.lower()
+                    if any(word in _item for word in filter_text.split()):
+                        self.listbox_client_selection.insert(tk.END, looping_item)
+        # self.listbox_client_selection.activate(0)
