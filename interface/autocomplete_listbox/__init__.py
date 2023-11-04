d6fc398ede371010c8aa99ebe2d029661d644e44
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-11-03 23:26:19 -0300

    autocomplete interface + gias login improvement

--- interface/autocomplete_listbox/__init__.py
+++ interface/autocomplete_listbox/__init__.py
@@ -6,8 +6,16 @@ from CTkListbox import *
 
 
 class AutoCompleteListbox:
-    def __init__(self, frame: tk.Frame, listbox_client_selection: Union[tk.Listbox, CTkListbox], allowed_clients:list,
-                 grid_elements=True):
+    def __init__(self, frame: tk.Frame, listbox_client_selection: Union[tk.Listbox, CTkListbox], allowed_clients: list,
+                 can_create_grid=True):
+        """
+        Initialize an AutoCompleteListbox.
+
+        :param frame: The tkinter Frame to contain the AutoCompleteListbox.
+        :param listbox_client_selection: The tkinter Listbox or CTkListbox for client selection.
+        :param allowed_clients: A list of allowed client items.
+        :param can_create_grid: (Optional) Whether the entry field should be created and placed in the grid. Defaults to True.
+        """
         self.listbox_client_selection = listbox_client_selection
         self.allowed_clients = allowed_clients
         self.original_items = self.allowed_clients  # cópia dos elementos originais
@@ -15,13 +23,19 @@ class AutoCompleteListbox:
         for item in self.allowed_clients:
             self.listbox_client_selection.insert(tk.END, item)
 
-        self.entry = ctk.CTkEntry(frame)
+        self.entry = ctk.CTkEntry(frame,
+                                  placeholder_text=f"Pesquisar:",
+                                  height=50, font=ctk.CTkFont(size=20, weight="bold"))
         self.entry.bind("<KeyRelease>", self.filter_listbox)
         self.entry.bind("<KeyRelease-Return>", lambda event: self.listbox_client_selection.activate(0))
-        if grid_elements:
-            self.entry.grid()
+
+        if can_create_grid:
+            self.entry.grid(sticky='nsew')
 
     def filter_listbox(self, event):
+        if 37 <= event.keycode <= 40:
+           return
+
         filter_text = self.entry.get().lower()
         try:
             self.listbox_client_selection.delete(0, tk.END)  # Limpar a lista
