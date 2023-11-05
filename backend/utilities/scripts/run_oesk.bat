0a9abba1b5283fdaa3530a020e73259c95f0ab5f
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-11-05 15:38:07 -0300

    PYTHONPATH na raça

--- backend/utilities/scripts/run_oesk.bat
+++ backend/utilities/scripts/run_oesk.bat
@@ -4,8 +4,10 @@ setlocal
 set "VENV_DIR=O:\HACKING\MY_PROJECTS\oesk\venv"
 set "MAIN_SCRIPT=O:\HACKING\MY_PROJECTS\oesk\main.py"
 
+set PYTHONPATH=O:\HACKING\MY_PROJECTS\oesk\backend;O:\HACKING\MY_PROJECTS\oesk\backend
+REM $env:PYTHONPATH += ";O:\HACKING\MY_PROJECTS\oesk\backend"
+
 call "%VENV_DIR%\Scripts\activate"    REM Ative o ambiente virtual
-call pip install .                    REM instala dependencias
 python "%MAIN_SCRIPT%"                REM Execute o arquivo main.py
 pause
 endlocal
