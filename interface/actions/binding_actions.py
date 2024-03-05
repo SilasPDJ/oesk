485969cc9556c7993a332fde1a5e23aeee22bd38
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2024-03-05 18:26:21 -0300

    init appian

--- interface/actions/binding_actions.py
+++ interface/actions/binding_actions.py
@@ -6,7 +6,7 @@ import subprocess
 import pandas as pd
 
 from backend.utilities.default.sets import FileOperations
-from backend.repository import MainEmpresasRepository, ClientComptsRepository
+from backend.repository import ClientComptsRepository
 from interface.settings import AppSettings
 
 
