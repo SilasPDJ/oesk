5a176fa14a0fdf29e6efa86ac40de97a4ea0fb03
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2024-04-06 11:32:01 -0300

    appian compts data

--- backend/utilities/services/api/__init__.py
+++ backend/utilities/services/api/__init__.py
@@ -1,12 +1,41 @@
+import json
+
 import requests
+from typing import Union
 
 
-def acessar_api_default(url: str, token: str, params: {}):
-    payload = {}
+def _validate_dict_param(arg: Union[dict, None]) -> dict: return dict() if not arg else arg
+
+
+def _execute_request(method, url, params=None, data=None, headers=None):
+    if method == "GET":
+        return requests.request(method, url, params=params, headers=headers, data=data)
+    elif method == "POST":
+        return requests.post(url, headers=headers, params=params, json=data)
+    else:
+        return {"text": ''}
+
+
+def acessar_api_default(url: str, token: str, method="GET", params=None, data=None, headers=None):
+    params = _validate_dict_param(params)
+    data = _validate_dict_param(data)
+
     headers = {
-        'Authorization': f'Bearer {token}'
+        'Authorization': f'Bearer {token}',
+        **_validate_dict_param(headers)
     }
-    response = requests.request("GET", url, params=params, headers=headers, data=payload)
-
+    response = _execute_request(method, url, params=params, data=data, headers=headers)
     print(response.text)
-    return response.json()
+    try:
+        return response.json()
+    except Exception as e:
+        return response
+
+
+if __name__ == '__main__':
+    test = _validate_dict_param({1: "a"})
+    test2 = _validate_dict_param(None)
+    # test3 = validate_dict_param("")
+    print(test)
+    print(test2)
+    # print(test3)
