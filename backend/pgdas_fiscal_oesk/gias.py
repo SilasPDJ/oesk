cffbea9b9856fe5c48cbf3d10ead7e4744ea7bf4
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2024-10-14 01:18:43 -0300

    gias new login fix

--- backend/pgdas_fiscal_oesk/gias.py
+++ backend/pgdas_fiscal_oesk/gias.py
@@ -15,7 +15,11 @@ class GIA(WDShorcuts):
     file_operations = FileOperations()
 
     def __init__(self, *args, compt, first_compt=None):
+        self.compt = compt
+        self.first_compt = first_compt
         __r_social, __ecac, login, senha = args
+        self.login, self.senha = login, senha
+        self.__ecac = __ecac
 
         # __anexo,  __valor_n_ret, __valor_ret, already_declared
 
@@ -29,17 +33,102 @@ class GIA(WDShorcuts):
         # drivers declarados
         # menuX, menuY = 20, 27
         self.menuX, self.menuY = 26, 31
+        # self.GIA()
 
+        # if certificado...
+        self.exec_gia_software_actions()
+
+        if not self.file_operations.certifs_exist(self.client_path, 'ReciboGIA', 1):
+            for loop_compt in ate_atual_compt(self.compt, self.first_compt):
+                self.driver = pgdas_driver(self.client_path)
+                self.driver.set_page_load_timeout(30)
+                super().__init__(self.driver)
+                self.exec_browrser_actions(loop_compt)
+
+    def exec_browrser_actions(self, loop_compt: str):
+        driver = self.driver
+        driver.get(
+            'https://www3.fazenda.sp.gov.br/CAWEB/Account/Login.aspx')
+        self.click_ac_elementors(driver.find_element(By.ID, 'ConteudoPagina_ImageButton1'))
+
+        login_url = ''
+        while driver.current_url == login_url or login_url == '':
+            if login_url != '':
+                self.driver.refresh()
+            login_url = driver.current_url
+            try:
+                llg = driver.find_element(By.ID,
+                                          'txtUsuario')
+                llg.clear()
+                llg.send_keys(self.login)
+
+                ssn = driver.find_element(By.XPATH,
+                                          "//input[@type='password']")
+                ssn.clear()
+                ssn.send_keys(self.senha)
+                # self.send_keys_anywhere(Keys.TAB)
+                # self.send_keys_anywhere(Keys.ENTER)
+                self._win_capt()
+                button = driver.find_element(By.NAME, "btnClaimsIdentity")
+                driver.execute_script("arguments[0].removeAttribute('disabled');", button)
+                button.click()
+                # print('pressione f9 p/ continuar após captcha')
+                # press_key_b4('f9')
+            except Exception as e:
+                driver.refresh()
+        cont = 0
+        while True:
+            try:
+                print(f"tentaiva {cont + 1}")
+                self.click_elements_by_tt('Cadastrar mais tarde')
+                break
+            except Exception as e:
+                print("Tentando clicar em cadastrar mais tarde")
+                cont += 1
+                if cont == 5:
+                    break
+        # enter entrar
+        self.webdriverwait_el_by(
+            By.LINK_TEXT, 'Guia de Informação (Arts. 253-254 RICMS/00)').click()
+        self.webdriverwait_el_by(By.LINK_TEXT, 'Envio de GIA').click()
+        # self.webdriverwait_el_by(By.PARTIAL_LINK_TEXT,
+        #                          'Documentos Fiscais (Normal, Substit.e Coligida)').click()
+        driver_clicks = driver.find_elements(By.XPATH,
+                                             "//input[@type='file']")
+
+        driver_clicks[0].send_keys(self.clieninput_filepath())
+        driver.find_elements(By.XPATH,
+                             "//input[@type='button']")[0].click()
+        try:
+            driver.switch_to.alert.accept()
+        except NoAlertPresentException:
+            print('Sem alerta')
+        """
+            bt_imprime = driver.find_element(By.CSS_SELECTOR, '[alt="Imprimir"]')
+            self.exec_list(click=bt_imprime, enter=pygui)
+            print('Glória a Deus f7 p continuar')
+            press_key_b4('f7')
+            """
+        png_name = 'GiaScreenShoot.png'
+        self.driver.save_screenshot(
+            os.path.join(self.client_path, png_name))
+        # convert_img2pdf is only joining both or NOT joining both...
+        self.file_operations.convert_img2pdf(
+            png_name,
+            f'ReciboGIA_{loop_compt}.pdf', self.client_path)
+        driver.close()
+        sleep(5)
+        # pygui.hotkey('enter')
+        # ############################################ parei daqui
+
+    def exec_gia_software_actions(self):
         def fecha_janela_contribuintes_gia():
             sleep(1)
             pygui.click(1322, 333, duration=.5)
             pygui.hotkey('left', 'enter')
 
-        # self.GIA()
-
-        # if certificado...
         if len(self.file_operations.files_get_anexos_v4(self.client_path, 'sfz')) < 1:
-            for loop_compt in ate_atual_compt(compt, first_compt):
+            for loop_compt in ate_atual_compt(self.compt, self.first_compt):
                 janelas_gias = pygui.getWindowsWithTitle('GIA')
                 for win in janelas_gias:
                     if win.title == 'GIA':
@@ -51,8 +140,8 @@ class GIA(WDShorcuts):
                     self.abre_programa(self.get_env_for_path(
                         '\\Desktop\\GIA.exe'), path=True)
 
-                IE = __ecac
-                my_print = login
+                IE = self.__ecac
+                my_print = self.login
                 print(my_print)
                 # pygui.hotkey('alt', 'tab')
                 print(IE)
@@ -77,82 +166,6 @@ class GIA(WDShorcuts):
                 pygui.hotkey('enter')
                 self.save_novagia()
 
-        if not self.file_operations.certifs_exist(self.client_path, 'ReciboGIA', 1):
-            for loop_compt in ate_atual_compt(compt, first_compt):
-                self.driver = driver = pgdas_driver(self.client_path)
-                super().__init__(self.driver)
-                driver.get(
-                    'https://www3.fazenda.sp.gov.br/CAWEB/Account/Login.aspx')
-                login_url = driver.current_url
-                while driver.current_url == login_url:
-                    try:
-                        llg = driver.find_element(By.ID,
-                                                  'ConteudoPagina_txtUsuario')
-                        llg.clear()
-                        llg.send_keys(login)
-
-                        ssn = driver.find_element(By.XPATH,
-                                                  "//input[@type='password']")
-                        ssn.clear()
-                        ssn.send_keys(senha)
-                        # self.send_keys_anywhere(Keys.TAB)
-                        # self.send_keys_anywhere(Keys.ENTER)
-                        self._win_capt()
-                        button = driver.find_element(By.ID, "ConteudoPagina_btnAcessar")
-                        driver.execute_script("arguments[0].removeAttribute('disabled');", button)
-                        button.click()
-                        # print('pressione f9 p/ continuar após captcha')
-                        # press_key_b4('f9')
-                    except Exception as e:
-                        driver.refresh()
-                cont = 0
-                while True:
-                    try:
-                        print(f"tentaiva {cont + 1}")
-                        sleep(7)
-                        self.click_elements_by_tt('Cadastrar mais tarde')
-                        break
-                    except Exception as e:
-                        print("Tentando clicar em cadastrar mais tarde")
-                        cont += 1
-                        if cont == 5:
-                            break
-                # enter entrar
-                self.webdriverwait_el_by(
-                    By.LINK_TEXT, 'Guia de Informação (Arts. 253-254 RICMS/00)').click()
-                self.webdriverwait_el_by(By.LINK_TEXT, 'Envio de GIA').click()
-                # self.webdriverwait_el_by(By.PARTIAL_LINK_TEXT,
-                #                          'Documentos Fiscais (Normal, Substit.e Coligida)').click()
-                sleep(3)
-                driver_clicks = driver.find_elements(By.XPATH,
-                                                     "//input[@type='file']")
-
-                driver_clicks[0].send_keys(self.clieninput_filepath())
-                driver.find_elements(By.XPATH,
-                                     "//input[@type='button']")[0].click()
-                try:
-                    driver.switch_to.alert.accept()
-                except NoAlertPresentException:
-                    print('Sem alerta')
-                sleep(5)
-                """
-                    bt_imprime = driver.find_element(By.CSS_SELECTOR, '[alt="Imprimir"]')
-                    self.exec_list(click=bt_imprime, enter=pygui)
-                    print('Glória a Deus f7 p continuar')
-                    press_key_b4('f7')
-                    """
-                png_name = 'GiaScreenShoot.png'
-                self.driver.save_screenshot(
-                    os.path.join(self.client_path, png_name))
-                # convert_img2pdf is only joining both or NOT joining both...
-                self.file_operations.convert_img2pdf(
-                    png_name,
-                    f'ReciboGIA_{loop_compt}.pdf', self.client_path)
-                driver.close()
-                sleep(5)
-                # pygui.hotkey('enter')
-                # ############################################ parei daqui
-
     def _win_capt(self):
         # recaptcha-token
         driver = self.driver
@@ -168,24 +181,21 @@ class GIA(WDShorcuts):
         if solved_resposta != 0:
             print(solved_resposta)
 
-            for frame_str in ["#divRecaptcha > div > div > iframe", 'body > div > div:nth-child(4) > iframe']:
-                try:
-                    driver.switch_to.frame(driver.find_element(By.CSS_SELECTOR, frame_str))
-                    secret_input = driver.find_element(By.ID, "recaptcha-token")
-                    driver.execute_script("arguments[0].setAttribute('value',arguments[1])", secret_input,
-                                          solved_resposta)
-                    driver.switch_to.default_content()
+            # EVENTVALIDATION = self.driver.find_element(By.ID, '__EVENTVALIDATION')
+            def set_solver_value(arg_id):
+                driver.execute_script("arguments[0].setAttribute('value',arguments[1])",
+                                      self.driver.find_element(By.ID, arg_id),
+                                      solved_resposta)
 
-                    for el_idname in ["g-recaptcha-response", "g-recaptcha-response-1"]:
-                        try:
-                            textarea_element = driver.find_element(By.ID, el_idname)
-                            driver.execute_script("arguments[0].innerHTML = arguments[1];", textarea_element,
-                                                  solved_resposta)
-                        except Exception as e:
-                            print(el_idname, "não existe")
-                except Exception as e:
-                    pass
+            for css_selector in ["body > div:nth-child(8) > div:nth-child(4) > iframe",
+                                 "#html_element > div > div > iframe"]:
+                driver.switch_to.frame(driver.find_element(By.CSS_SELECTOR, css_selector))
+                set_solver_value('recaptcha-token')
+                driver.switch_to.default_content()
 
+            textarea_element = driver.find_element(By.ID, 'g-recaptcha-response')
+            driver.execute_script("arguments[0].innerHTML = arguments[1];", textarea_element,
+                                  solved_resposta)
             if solver.err_string != '':
                 print(solver.err_string)
 
